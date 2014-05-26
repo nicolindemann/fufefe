5177490cc15364421aed91cd5785b41c99414e78
Nico Lindemann <nl@apprupt.com>
2014-05-26 03:28:40 +0200

    simply

--- js/main.js
+++ js/main.js
@@ -1,47 +1,3 @@
-
-
-
-
-
-var date = new Date();
-var message,
-timeoutTimer,
-timer;
-
-function addRow(table,field,val){
-  var tr = $('<tr />').appendTo( $(table));
-  tr.append($('<td />').css('font-weight','bold').text(field)).append($('<td />').text(val));
-
-}
-
-function throwMessage(msg,duration){
-
-  window.clearTimeout(timeoutTimer);
-  timeoutTimer = window.setTimeout(function(){
-    message.fadeOut(function(){
-      message.remove();
-    });
-  },duration || 2000);
-  $(message).remove();
-  message = $('<div />').html(msg).css({
-    margin:0,
-    padding:10,
-    background: "#000",
-    opacity:0.7,
-    position:"fixed",
-    top:10,
-    right:10,
-    fontFamily: 'Tahoma' ,
-    color:'#fff',
-    fontSize:12,
-    borderRadius:12,
-    width:'auto',
-    height:'auto',
-    textAlign:'center',
-    textDecoration:'none'
-  }).hide().fadeIn().appendTo('body');
-}
-
 function iframeLoad(iframe) {
 
   var body = $(iframe).contents().find('body');
@@ -63,7 +19,6 @@ $(function(){
 
   var iframe,d;
 
-  $('#getscreenshot').click(function(e){
     e.preventDefault();
     $(this).prop('disabled',true);
     var url = "http://blog.fefe.de"
@@ -88,36 +43,13 @@ $(function(){
         $('body').append(iframe);
 		console.log(html);
 		
-		iframe.src = html
+		iframe.src = html;
+		iframe.style.display = 'none';
         $(iframe).load(iframeLoad.bind(null, iframe));
-		
-       /* d = iframe.contentWindow.document;
 
-        d.open();
-
-
-        $('base').attr('href',urlParts.protocol+"//"+urlParts.hostname+"/" + urlParts.pathname);
-        html = html.replace("<head>","<head><base href='"+urlParts.protocol+"//"+urlParts.hostname+"/" + urlParts.pathname + "'  />");
-		
-	    if ($("#disablejs").prop('checked')){
-          html = html.replace(/\<script/gi,"<!--<script");
-          html = html.replace(/\<\/script\>/gi,"<\/script>-->");
-        }
-		
-
-        d.write(html);
-        //d.close();
-		*/
       }
 
 
     });
-
-
-  });
-
-
-
-
 
 });
