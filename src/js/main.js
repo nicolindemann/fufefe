3dc1ea678010224da162f85692293cc92b60d33c
nicolindemann <nico.lindemann@proux.de>
2014-06-01 00:59:05 +0000

    change stuff

--- src/js/main.js
+++ src/js/main.js
@@ -206,7 +206,7 @@ $(function () {
             xhr2: false,
             url: urlParts.href
         },
-        url: "http://" + window.location.host + "/proxy/index.php",
+        url: "http://" + window.location.host + "/index.php",
         dataType: "jsonp",
         success: function (html) {
             iframe = document.createElement('iframe');
